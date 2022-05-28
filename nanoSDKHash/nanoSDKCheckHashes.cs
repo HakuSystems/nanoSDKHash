9bbe964416c609fddc3678e607aa3ba0aebccdb4
HakuSystems <66133638+HakuSystems@users.noreply.github.com>
2022-05-28 16:32:25 +0200

    Add project files.

---
+++ nanoSDKHash/nanoSDKCheckHashes.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEditor;
+
+namespace nanoSDKHash
+{
+    public class nanoSDKCheckHashes
+    {
+        public static void CheckHashes()
+        {
+            string path = $"Assets{Path.DirectorySeparatorChar}VRCSDK{Path.DirectorySeparatorChar}nanoSDK";
+            Uri serverUrl = new Uri("https://www.nanosdk.net/download/Hash/hashes.txt");
+
+            var files = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories)
+                                 .OrderBy(p => p).ToList();
+
+            var hashFile = File.ReadAllText($"{path}{Path.DirectorySeparatorChar}hashes.txt");
+
+
+            foreach (var file in files)
+            {
+                if (!file.EndsWith(".cs"))
+                {
+                    continue;
+                }
+                var md5 = MD5.Create();
+                var hash = md5.ComputeHash(File.ReadAllBytes(file));
+                var result = BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+                //Debug.Log(file + " : " + result);
+                if (!hashFile.Contains(result))
+                {
+                    Debug.LogError("File is not the same: " + file + " : " + result);
+                    if (EditorUtility.DisplayDialog("Error", "Manipulation Detected, Please dont Manipulate nanoSDK Code!", "OK"))
+                    {
+                        
+                        foreach (var filePath in files)
+                        {
+                            if (filePath.EndsWith(".cs"))
+                            {
+                                File.Delete(filePath);
+                            }
+                        }
+                        //Exit Unity
+                        EditorApplication.Exit(0);
+                    }
+                }
+
+            }
+
+        }
+    }
+}
---
+++ nanoSDKHash/nanoSDKGenerateHashes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace nanoSDKHash
+{
+    public class nanoSDKGenerateHashes
+    {
+        public static void GenerateHash()
+        {
+            string path = $"Assets{Path.DirectorySeparatorChar}VRCSDK{Path.DirectorySeparatorChar}nanoSDK";
+            var files = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories)
+                                 .OrderBy(p => p).ToList();
+            File.WriteAllText($"{path}{Path.DirectorySeparatorChar}hashes.txt", "");
+            foreach (var file in files)
+            {
+                if (!file.EndsWith(".cs"))
+                {
+                    continue;
+                }
+                var md5 = MD5.Create();
+                var hash = md5.ComputeHash(File.ReadAllBytes(file));
+                var result = BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+                Debug.Log(file + " : " + result);
+
+                File.AppendAllText($"{path}{Path.DirectorySeparatorChar}hashes.txt", $"{result}\n");
+            }
+        }
+    }
+}
